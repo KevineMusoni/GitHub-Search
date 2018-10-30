5789bfd73ba801815b5932ca48bd308297f9209b
Kevine Musoni <kevinenicky@gmail.com>
2018-10-30 10:44:25 +0300

    default api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,6 @@ export const environment = {
   // api github url
   apiUrl: 'https://api.github.com/users/daneden?access_token=3767e07a8cfa5a0ab04e6c1dac7d4568bdbc6817',
   apiKey: '3767e07a8cfa5a0ab04e6c1dac7d4568bdbc6817',
+  // default api
   defaultapiUrl: 'https://api.github.com/users/KevineMusoni?access_token=3767e07a8cfa5a0ab04e6c1dac7d4568bdbc6817'
 };
-
-
