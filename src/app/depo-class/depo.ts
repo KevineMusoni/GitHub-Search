5f88b1fd649ecb41b5921d4b617870791fd37118
Kevine Musoni <kevinenicky@gmail.com>
2018-10-30 10:22:58 +0300

    repository class edited

--- src/app/depo-class/depo.ts
+++ src/app/depo-class/depo.ts
@@ -1,6 +1,5 @@
+// REPOSITORY CLASS
 export class Repo {
   avatar_url: string;
-  // public_repos: any;
-  // followers: string;
   constructor(public public_repos: string, public followers: string, public login: string, avatar_url: any, public following: string) { }
 }
